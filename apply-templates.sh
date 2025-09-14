2b416bdd2fa58f2dac081fd73ddeddf7f054b492
howcroft <howcroft@gmail.com>
2025-09-14 12:23:11 +0100

    changed the apply-templates and linux template

--- apply-templates.sh
+++ apply-templates.sh
@@ -8,7 +8,7 @@ if [ -n "${BASHBREW_SCRIPTS:-}" ]; then
 	jqt="$BASHBREW_SCRIPTS/jq-template.awk"
 elif [ "$BASH_SOURCE" -nt "$jqt" ]; then
 	# https://github.com/docker-library/bashbrew/blob/master/scripts/jq-template.awk
-	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/9f6a35772ac863a0241f147c820354e4008edf38/scripts/jq-template.awk'
+	curl -sLo "$jqt" 'https://github.com/docker-library/bashbrew/raw/9f6a35772ac863a0241f147c820354e4008edf38/scripts/jq-template.awk'
 fi
 
 if [ "$#" -eq 0 ]; then
@@ -58,7 +58,7 @@ for version; do
 
 		{
 			generated_warning
-			gawk -f "$jqt" "$template"
+			awk -f "$jqt" "$template"
 		} > "$version/$dir/Dockerfile"
 	done
 done
